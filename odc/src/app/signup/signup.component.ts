d6bc08b25051e73359aa7409d27b1f79e764357f
Chad Baily <chadbaily27@gmail.com>
2018-05-30 22:30:35 -0400

    Working on getting paypal button working

--- odc/src/app/signup/signup.component.ts
+++ odc/src/app/signup/signup.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: "app-signup",
-  templateUrl: "./signup.component.html"
+  selector: 'app-signup',
+  templateUrl: './signup.component.html'
 })
 export class SignupComponent implements OnInit {
   constructor(private _formBuilder: FormBuilder) {}
 
-  value = "";
+  value = '';
   hide = true;
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
@@ -16,45 +16,47 @@ export class SignupComponent implements OnInit {
   fourthFormGroup: FormGroup;
 
   genders = [
-    { value: "male", viewValue: "Male" },
-    { value: "female", viewValue: "Female" }
+    { value: 'male', viewValue: 'Male' },
+    { value: 'female', viewValue: 'Female' }
   ];
 
   studentOptions = [
-    { value: "yes", viewValue: "Yes" },
-    { value: "no", viewValue: "No" }
+    { value: 'yes', viewValue: 'Yes' },
+    { value: 'no', viewValue: 'No' }
   ];
 
   memberships = [
-    { value: "semester", viewValue: "Semester (5-Month) Membership ($30)" },
-    { value: "year", viewValue: "Yearly (12-month) Membership ($50)" }
+    { value: 'semester', viewValue: 'Semester (5-Month) Membership ($30)' },
+    { value: 'year', viewValue: 'Yearly (12-month) Membership ($50)' }
   ];
 
   ngOnInit() {
     this.firstFormGroup = this._formBuilder.group({
-      email: ["", Validators.required],
-      password: ["", Validators.required]
+      email: ['', Validators.required],
+      password: ['', Validators.required]
     });
     this.secondFormGroup = this._formBuilder.group({
-      firstName: ["", Validators.required],
-      lastName: ["", Validators.required],
-      gender: ["", Validators.required],
-      birthDate: ["", Validators.required],
-      student: ["", Validators.required],
-      address: ["", Validators.required],
-      city: ["", Validators.required],
-      state: ["", Validators.required],
-      zip: ["", Validators.required]
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      gender: ['', Validators.required],
+      birthDate: ['', Validators.required],
+      student: ['', Validators.required],
+      address: ['', Validators.required],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      zip: ['', Validators.required]
     });
     this.thirdFormGroup = this._formBuilder.group({
-      waiver: ["", Validators.required]
+      waiver: ['', Validators.required]
     });
     this.fourthFormGroup = this._formBuilder.group({
-      plan: ["", Validators.required]
+      plan: ['', Validators.required]
     });
   }
 
-  createUserData() {
-    this.firstFormGroup.controls.email.value;
+  onSubmit() {
+    if (this.fourthFormGroup.valid) {
+      console.log('Form Submitted!');
+    }
   }
 }
